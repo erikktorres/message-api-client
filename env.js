032942c484e44e41115acfa0fff781479438514c
Jamie <jamie@tidepool.org>
2014-01-30 11:28:40 +1300

    jshint across repo

--- env.js
+++ env.js
@@ -3,22 +3,23 @@
 
 // == BSD2 LICENSE ==
 // Copyright (c) 2014, Tidepool Project
-// 
+//
 // This program is free software; you can redistribute it and/or modify it under
 // the terms of the associated License, which is identical to the BSD 2-Clause
 // License as published by the Open Source Initiative at opensource.org.
-// 
+//
 // This program is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 // FOR A PARTICULAR PURPOSE. See the License for more details.
-// 
+//
 // You should have received a copy of the License along with this program; if
 // not, you can obtain one from Tidepool Project at tidepool.org.
 // == BSD2 LICENSE ==
 
+'use strict';
+
 var fs = require('fs');
 
-'use strict';
 module.exports = (function() {
   var env = {};
 
@@ -26,13 +27,13 @@ module.exports = (function() {
 
   //groups
   env.groupsApiService = process.env.GROUPS_API_SERVICE;
-  
+
   if (env.groupsApiService == null) {
     throw new Error('Must specify a GROUPS_API_SERVICE in your environment.');
   }
   //messages
   env.messagesApiService = process.env.MESSAGES_API_SERVICE;
-  
+
   if (env.messagesApiService == null) {
     throw new Error('Must specify a MESSAGES_API_SERVICE in your environment.');
   }
--- test/client_test.js
+++ test/client_test.js
@@ -1,14 +1,14 @@
 // == BSD2 LICENSE ==
 // Copyright (c) 2014, Tidepool Project
-// 
+//
 // This program is free software; you can redistribute it and/or modify it under
 // the terms of the associated License, which is identical to the BSD 2-Clause
 // License as published by the Open Source Initiative at opensource.org.
-// 
+//
 // This program is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 // FOR A PARTICULAR PURPOSE. See the License for more details.
-// 
+//
 // You should have received a copy of the License along with this program; if
 // not, you can obtain one from Tidepool Project at tidepool.org.
 // == BSD2 LICENSE ==
@@ -19,90 +19,90 @@ var mockedRequest = require('./mock/mockRequest');
 
 describe('messages client', function() {
 
-    var client;
+  var client;
 
-    before(function(){
-        var groupsHostGetter = require('./mock/mockHakkenWatcher')('localhost','8080');
-        var messagesHostGetter = require('./mock/mockHakkenWatcher')('localhost','8082');
-        client = require('../lib/client')(groupsHostGetter, messagesHostGetter, mockedRequest);
+  before(function(){
+    var groupsHostGetter = require('./mock/mockHakkenWatcher')('localhost','8080');
+    var messagesHostGetter = require('./mock/mockHakkenWatcher')('localhost','8082');
+    client = require('../lib/client')(groupsHostGetter, messagesHostGetter, mockedRequest);
+  });
+
+  it('returns groups and associated messages for user', function(done) {
+
+    client.getUsersMessages('1234','fakeit',function(err,userMessages){
+      if(err) done(err);
+
+      expect(userMessages).to.exist;
+      expect(userMessages.groups).to.exist;
+
+      var oneGroup = userMessages.groups[0];
+      expect(oneGroup.messages).to.exist;
+
+      done();
     });
 
-    it('returns groups and associated messages for user', function(done) {
+  });
 
-        client.getUsersMessages('1234','fakeit',function(err,userMessages){
-            if(err) done(err);
+  it('returns two groups', function(done) {
 
-            expect(userMessages).to.exist;
-            expect(userMessages.groups).to.exist;
+    client.getUsersMessages('1234','fakeit',function(err,userMessages){
+      if(err) done(err);
 
-            var oneGroup = userMessages.groups[0];
-            expect(oneGroup.messages).to.exist;
+      expect(userMessages.groups).to.exist;
+      expect(userMessages.groups.length).to.equal(2);
 
-            done();
-        });
-
+      done();
     });
 
-    it('returns two groups', function(done) {
+  });
 
-        client.getUsersMessages('1234','fakeit',function(err,userMessages){
-            if(err) done(err);
+  it('returns three messages for the first group', function(done) {
 
-            expect(userMessages.groups).to.exist;
-            expect(userMessages.groups.length).to.equal(2);
+    client.getUsersMessages('1234','fakeit',function(err,userMessages){
+      if(err) done(err);
 
-            done();
-        });
-
+      var oneGroup = userMessages.groups[0];
+      expect(oneGroup.messages.length).to.equal(3);
+      done();
     });
 
-    it('returns three messages for the first group', function(done) {
+  });
 
-        client.getUsersMessages('1234','fakeit',function(err,userMessages){
-            if(err) done(err);
+  it('returns one message for the second group', function(done) {
 
-            var oneGroup = userMessages.groups[0];
-            expect(oneGroup.messages.length).to.equal(3);
-            done();
-        });
+    client.getUsersMessages('1234','fakeit',function(err,userMessages){
+      if(err) done(err);
 
+      var oneGroup = userMessages.groups[1];
+      expect(oneGroup.messages.length).to.equal(1);
+      done();
     });
 
-    it('returns one message for the second group', function(done) {
-
-        client.getUsersMessages('1234','fakeit',function(err,userMessages){
-            if(err) done(err);
-
-            var oneGroup = userMessages.groups[1];
-            expect(oneGroup.messages.length).to.equal(1);
-            done();
-        });
-
-    });
+  });
 
 });
 
 describe('messages client cannot talk to hosts', function() {
 
-    var client;
+  var client;
 
-    before(function(){
+  before(function(){
 
-        var emptyGetter = require('./mock/mockEmptyHakkenWatcher')();
+    var emptyGetter = require('./mock/mockEmptyHakkenWatcher')();
 
-        client = require('../lib/client')(emptyGetter, emptyGetter, mockedRequest);
-    });
+    client = require('../lib/client')(emptyGetter, emptyGetter, mockedRequest);
+  });
 
-    it('returns an error code and message as the host cannot be found', function(done) {
+  it('returns an error code and message as the host cannot be found', function(done) {
 
-        client.getUsersMessages('1234','fakeit',function(err,userMessages){
+    client.getUsersMessages('1234','fakeit',function(err,userMessages){
 
-            expect(err.statusCode).to.equal(503);
-            expect(err.message).to.exist;
-            done();           
-            
-        });
+      expect(err.statusCode).to.equal(503);
+      expect(err.message).to.exist;
+      done();
 
     });
+
+  });
 
 });
